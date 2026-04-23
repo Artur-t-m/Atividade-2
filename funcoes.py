6d787e442605fbaa728d8a254fb4c931719bcdb2
Artur-t-m <arturtm@al.insper.edu.br>
2026-04-23 10:40:44 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -28,7 +28,8 @@ def calcula_pontos_soma(lista_dados):
     for i in range(len(lista_dados)):
         soma += lista_dados[i]
     return soma
-def calcula_pontos_sequencia_baixa(lista_dados):
+
+def calcula_pontos_sequencia_baixa(lista_dados): #irmão, deve ter um jeito mais fácil, mas não me importo, tá aí.
     dict_dados = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
     for dado in lista_dados:
         dict_dados[dado] += 1
@@ -40,3 +41,24 @@ def calcula_pontos_sequencia_baixa(lista_dados):
         return 15
     else:
         return 0
+    
+def calcula_pontos_sequencia_alta(lista_dados): 
+    dict_dados = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
+    for dado in lista_dados:
+        dict_dados[dado] += 1
+    if dict_dados[1] >= 1 and dict_dados[2] >= 1 and dict_dados[3] >= 1 and dict_dados[4] >= 1 and dict_dados[5] >= 1:
+        return 30
+    elif dict_dados[2] >= 1 and dict_dados[3] >= 1 and dict_dados[4] >= 1 and dict_dados[5] >= 1 and dict_dados[6] >= 1:
+        return 30
+    else:
+        return 0
+    
+def calcula_pontos_full_house(lista_dados): #Ou era dicionário ou era 1000000 de elifs.
+    dict_dados = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
+    for dado in lista_dados:
+        dict_dados[dado] += 1
+    if 3 in dict_dados.values() and 2 in dict_dados.values():
+        soma = 0
+        for i in range(len(lista_dados)):
+            soma += lista_dados[i]
+        return soma 
