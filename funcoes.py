e4c53f48d87583f2f9f8905f7e23f61dfc26e895
Artur-t-m <arturtm@al.insper.edu.br>
2026-04-23 09:59:24 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -4,3 +4,9 @@ def rolar_dados(n_dados):
     for i in range(n_dados):
         resultado.append(random.randint(1, 6))
     return resultado
+
+def guardar_dado(dados_rolados, dados_no_estoque, dados_para_guardar):
+    dados_no_estoque.append(dados_rolados[dados_para_guardar])
+    del dados_rolados[dados_para_guardar]
+    lista_dados = [dados_rolados, dados_no_estoque]
+    return lista_dados
